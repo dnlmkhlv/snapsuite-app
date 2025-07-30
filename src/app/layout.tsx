dd0c5193830bad7266d415296d9e395a1b79dbb7
dnlmkhlv <dan18082@mail.ru>
2025-07-30 12:44:57 +0400

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -115,9 +115,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/favicon-16x16.png"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          rel="icon"
+          href="/favicon-32x32.png"
+          type="image/png"
+          sizes="32x32"
+        />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#5170FF" />
         <meta
           name="viewport"
